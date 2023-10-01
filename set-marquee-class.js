d1609985a9dc4f90cbaf2877070630f76a6e8e6a
Caian Ertl <hi@caian.org>
2023-10-01 00:58:18 -0300

    initial commit

---
+++ set-marquee-class.js
@@ -0,0 +1,17 @@
+const setMarqueeClass = (className, direction, interval) => {
+    const elements = document.getElementsByClassName(className);
+
+    setInterval(() => {
+        for (let i = 0, n = elements.length; i < n; i++) {
+            const text = elements[i].textContent;
+            elements[i].textContent = direction === 'backwards'
+                ? text.substring(1) + text.charAt(0)
+                : text.charAt(text.length - 1) + text.substring(0, text.length - 1);
+        }
+    }, interval);
+};
+
+setMarqueeClass('back-marquee', 'backwards', 15);
+setMarqueeClass('forw-marquee', 'forwards', 15);
+
+setMarqueeClass('moving-banner', 'backwards', 100);
